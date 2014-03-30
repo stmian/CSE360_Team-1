bc7608c50c69c8a72ef8d1daab0758ded0e9cde2
BrendenAZ <builtbybrenden@gmail.com>
2014-03-29 20:47:05 -0700

    Fixed database connection string
    Added specific database to connect to and removed missing import from
    old Main() file.

--- MVC/BeHealthy.java
+++ MVC/BeHealthy.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 //package behealthy;
 
 import javax.swing.JFrame;
@@ -11,74 +5,63 @@ import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import java.sql.*;
 
-/**
- *
- * @author Brenden
- */
 public class BeHealthy {
 
-	/**
-	 * @param args the command line arguments
-	 */
     static Connection conn = null;
-	static String user = "cse360";
+    static String user = "cse360";
     static String pass = "behealthy";
     static String dbClass = "com.mysql.jdbc.Driver";
-    static String dbDriver = "jdbc:mysql://mysql.builtbybrenden.com";
-    
-	public static void main(String[] args) {
-		// TODO code application logic here
-        
-        
+    static String dbDriver = "jdbc:mysql://mysql.builtbybrenden.com:3306/behealthy";
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
         try {
             Class.forName(dbClass).newInstance();
             System.out.println("driver loaded");
         } catch (Exception ex) {
             System.err.println(ex);
         }
-        
+
         try {
             conn = DriverManager.getConnection(dbDriver, user, pass);
-            
             System.out.println("connected");
-            
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
         }
-        
-        
-		JFrame mainFrame = new JFrame();
 
-		UserModel userModel = new UserModel("", 0, "", false);
-		UserController userController = new UserController(userModel);
-		JPanel userPanel = userController.getPanel();
+        JFrame mainFrame = new JFrame();
 
-		HealthModel healthModel = new HealthModel();
-		HealthController healthController = new HealthController(healthModel);
-		JPanel healthPanel = healthController.getPanel();
+        UserModel userModel = new UserModel("", 0, "", false);
+        UserController userController = new UserController(userModel);
+        JPanel userPanel = userController.getPanel();
 
-		ActivitiesModel activitiesModel = new ActivitiesModel();
-		ActivitiesController activitiesController = new ActivitiesController(activitiesModel);
-		JPanel activitiesPanel = activitiesController.getPanel();
+        HealthModel healthModel = new HealthModel();
+        HealthController healthController = new HealthController(healthModel);
+        JPanel healthPanel = healthController.getPanel();
 
-		HomeModel homeModel = new HomeModel();
-		HomeController homeController = new HomeController(homeModel);
-		JPanel homePanel = homeController.getPanel();
+        ActivitiesModel activitiesModel = new ActivitiesModel();
+        ActivitiesController activitiesController = new ActivitiesController(activitiesModel);
+        JPanel activitiesPanel = activitiesController.getPanel();
 
-		//Create tabbed pane
-		JTabbedPane mPane = new JTabbedPane();
-		mPane.addTab("Home", homePanel);
-		mPane.addTab("Activities", activitiesPanel);
-		mPane.addTab("Health", healthPanel);
-		mPane.addTab("Profile", userPanel);
-		
-		mainFrame = new JFrame();
-		mainFrame.setSize(500, 500);
-		mainFrame.setTitle("BeHealthy");
-		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.setResizable(false);
-		mainFrame.setVisible(true);
-		mainFrame.add(mPane);
-	} //main
+        HomeModel homeModel = new HomeModel();
+        HomeController homeController = new HomeController(homeModel);
+        JPanel homePanel = homeController.getPanel();
 
+        //Create tabbed pane
+        JTabbedPane mPane = new JTabbedPane();
+        mPane.addTab("Home", homePanel);
+        mPane.addTab("Activities", activitiesPanel);
+        mPane.addTab("Health", healthPanel);
+        mPane.addTab("Profile", userPanel);
+
+        mainFrame = new JFrame();
+        mainFrame.setSize(500, 500);
+        mainFrame.setTitle("BeHealthy");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setResizable(false);
+        mainFrame.setVisible(true);
+        mainFrame.add(mPane);
+    } //main
 } //BeHealthy
