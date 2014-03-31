eeac60bb0408f1a4ceb2b274bca3b36cc69e8a9e
BrendenAZ <builtbybrenden@gmail.com>
2014-03-30 21:04:06 -0700

    Stubbed out HealthModel and HealthMetric classes

--- MVC/HealthModel.java
+++ MVC/HealthModel.java
@@ -1,17 +1,43 @@
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class HealthModel {
+
     //=================== Private properties/methods ===================//
-    /*TODO: Enter private variables here e.e.: private String name; */
+    private class HealthMetric {
+
+        int id;
+        int typeId;
+        String typeName;
+        double duration;
+        Date date;
+
+        public HealthMetric(int id, int typeId, String typeName, double duration, Date date) {
+            this.id = id;
+            this.typeId = typeId;
+            this.typeName = typeName;
+            this.duration = duration;
+            this.date = date;
+        } //__constructor
+    } //HealthMetric
+
+    private ArrayList<HealthMetric> healthMetrics;
 
     //=================== Public properties/methods ====================//
     public HealthModel() {
+        // TODO: add database call to read all health metric records
+    } //__constructor
 
-    }
+    public void addHealthMetric(int typeId, double duration, Date date) {
+        // TODO: add database call
+    } //addHealthMetric
 
-    /*TODO: Enter public getters/setters here e.g.:
+    public void updateHealthMetric(int id, int typeId, double duration, Date date) {
+        // TODO: add database call
+    } //updateHealthMetric
 
-     public String getName() {
-     return name;
-     }
-     */
-}
+    public void removeHealthMetric(int id) {
+        // TODO: add database call
+    } //updateHealthMetric
+} //HealthModel
