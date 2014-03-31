a00a115325aabb2fa50f74d83f17c386afb85c21
rapratt <rphello101@ymail.com>
2014-03-30 23:00:21 -0700

    Database implementation on HealthMetrics
    Basically adapted all the activity stuff over to health Metrics.

--- MVC/HealthModel.java
+++ MVC/HealthModel.java
@@ -1,6 +1,13 @@
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+
+import ActivitiesModel.Activity;
 
 public class HealthModel {
 
@@ -26,18 +33,91 @@ public class HealthModel {
 
     //=================== Public properties/methods ====================//
     public HealthModel() {
-        // TODO: add database call to read all health metric records
+    	healthMetrics = new ArrayList();
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+        String[] temp;
+        Statement st;
+			try {
+				st = (BeHealthy.conn).createStatement();
+				ResultSet res = st.executeQuery("SELECT * FROM  activities");
+				temp = res.toString().split(" ");
+	            while (res.next()) {
+	                // TODO: Change this to add Activities to the collection without the add method. This is only used to add new records.
+	                addHealthMetric(Integer.parseInt(temp[0]), Double.parseDouble(temp[1]), dateFormatter.parse(temp[2]));
+	            	}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+            
+            
+            
+   
     } //__constructor
 
-    public void addHealthMetric(int typeId, double metric, Date date) {
-        // TODO: add database call
+    public boolean addHealthMetric(int typeId,double metric, Date date) {
+    	String name="";
+    	if(typeId==0)
+    	{
+    		name="Weight";
+    	}
+    	if(typeId==1)
+    	{
+    		name="Blood Pressure";
+    	}
+    	if(typeId==2)
+    	{
+    		name="Blood Sugar";
+    	}
+    	if(typeId==3)
+    	{
+    		name="Heart Rate";
+    	}
+        HealthMetric d = new HealthMetric(1,typeId,name,metric, date);
+        int i = -1;
+
+        try {
+            Statement st = (BeHealthy.conn).createStatement();
+
+            i = st.executeUpdate("");//TODO ADD APPROPRIATE SQL QUERY TO ADD ACTIVITY
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+
+        if (i == 1) {
+            healthMetrics.add(d);
+            return true;
+        } else {
+            return false;
+        }
+
     } //addHealthMetric
 
-    public void updateHealthMetric(int id, int typeId, double metric, Date date) {
+    public boolean updateHealthMetric(int id, int typeId, double metric, Date date) {
         // TODO: add database call
     } //updateHealthMetric
 
-    public void removeHealthMetric(int id) {
-        // TODO: add database call
+    public boolean removeHealthMetric(int id) {
+    	int i = -1;
+
+        try {
+            Statement st = (BeHealthy.conn).createStatement();
+
+            i = st.executeUpdate("");//ADD APPROPRIATE SQL QUERY TO REMOVE ACTIVITY
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+
+        if (i == 1) {
+            healthMetrics.remove(i);
+            return true;
+        } else {
+            return false;
+        }
+
     } //updateHealthMetric
 } //HealthModel
